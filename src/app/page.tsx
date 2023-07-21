55286d12f23a926fd52a15fc56607dccc4189e05
JuniorUbarana <elci.junior@gmail.com>
2023-07-21 12:00:40 -0300

    NewChat added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,56 @@ import { ChatArea } from "@/components/ChatArea";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Chat } from "@/types/Chat";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Sidebar from "@/components/Sidebar";
+import { v4 as uuidv4 } from 'uuid';
 
 const Page = () => {
 
   const [AILoading, setAILoading] = useState(false);
-  const [chatList, setChatList] = useState();
+  const [chatList, setChatList] = useState<Chat[]>([]);
+  const [chatActiveId, setChatActiveId] = useState<String>('');
   const [chatActive, setChatActive] = useState<Chat>();
   const [sidebarOpened, setSidebarOpened] = useState(false);
+
+  useEffect(() => {
+    setChatActive(chatList.find(item => item.id === chatActiveId));
+  }, [chatActiveId, chatList]);
 
   const openSidebar = () => setSidebarOpened(true);
   const closeSidebar = () => setSidebarOpened(false);
 
   const handleClearConversations = () => {
-
+    if(AILoading) return;
+    
+    setChatActiveId('');
+    setChatList([]);
   }
 
   const handleNewChat = () => {
-    
+    if(AILoading) return;
+
+    setChatActiveId('');
+    closeSidebar();
   }
 
-  const handleSendMessage = () => {
+  const handleSendMessage = (message: string) => {
+    if(!chatActiveId) {
+      //Creating new chat
+      var newChatId = uuidv4();
+      setChatList([{
+        id: newChatId,
+        title: message,
+        messages: [
+          { id: uuidv4(), author: 'me', body: message }
+        ]
+      }, ...chatList]);
 
+      setChatActiveId(newChatId);
+    } else {
+      //Updating existing chat
+      
+    }
   }
 
   return (
